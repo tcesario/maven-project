e98b55fbd40930e1221c961f287fcfb04dafeb31
Thiago Ceśario da Silva <thiago.cesario@mandic.net.br>
2020-06-10 21:31:34 -0300

    fix Greeter file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
-  public string greet(String someone) {
+  public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
